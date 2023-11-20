5270087c306033fb43a44527000c030633c0655f
IQRAR INAM <iqrarinam766@gmail.com>
2023-11-20 14:10:02 +0500

    MyPortfoliowebsite

---
+++ js/main.js
@@ -0,0 +1,15 @@
+myProject1 = () =>{
+    window.open('https://iqrar766.github.io/tindog.github.io/');
+}
+
+myProject2 = () =>{
+    window.open('https://iqrar766.github.io/iCoder.github.io/')
+}
+
+myGithub = () =>{
+    window.open('https://github.com/Iqrar766')
+}
+
+mySchool = () =>{
+    document.getElementById('popup').innerHTML ="Goto School";
+}
